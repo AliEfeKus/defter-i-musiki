a2f100c4077afcfccffe132250ae06b3549e9408
Ali Efe Kuş <plaklardannostalji@gmail.com>
2026-01-22 00:28:32 +0300

    data.js

---
+++ data.js
@@ -0,0 +1,29 @@
+const eserler = [
+    {
+        id: 1,
+        ad: "Yine bir Gülnihal",
+        makam: "Rast",
+        usul: "Semai",
+        bestekar: "Hammamizade İsmail Dede Efendi",
+        tur: "Şarkı",
+        notaLink: "#"
+    },
+    {
+        id: 2,
+        ad: "Fikrimin İnce Gülü",
+        makam: "Acemkürdi",
+        usul: "Semai",
+        bestekar: "İsmail Hakkı Bey",
+        tur: "Şarkı",
+        notaLink: "#"
+    },
+    {
+        id: 3,
+        ad: "Dönülmez Akşamın Ufkundayız",
+        makam: "Segah",
+        usul: "Ağır Aksak",
+        bestekar: "Münir Nurettin Selçuk",
+        tur: "Şarkı",
+        notaLink: "#"
+    }
+];
