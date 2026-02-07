3f65d5723eb94e3d01ecd21bed908475d3ab02cd
Ali Efe Kuş <plaklardannostalji@gmail.com>
2026-02-07 23:23:28 +0300

    Create eserler.js

---
+++ hamamizade-ismail-dede-efendi/eserler.js
@@ -0,0 +1,12 @@
+const eserler = [
+            { "id": 1, "eser": "A zalim kanıma girdin", "makam": "Hisar", "güftekâr": "Mechul", "bestekâr": "Hamâmîzâde İsmâil Dede Efendi", "usûl": "Aksak", "form": "Şarkı", "nota": "", "icrâ": "", "güfte": ""},
+            { "id": 1, "eser": "Açıl açıl gel efendim cihan bahar olsun ", "makam": "Hicaz Buselik", "güftekâr": "Mechul", "bestekâr": "Hamâmîzâde İsmâil Dede Efendi", "usûl": "Yürük Semai", "form": "Yürük Semai", "nota": "", "icrâ": "", "güfte": ""},
+            { "id": 1, "eser": "Açıldı lale-izarın dağ-ı derun", "makam": "Şehnaz", "güftekâr": "Mechul", "bestekâr": "Hamâmîzâde İsmâil Dede Efendi", "usûl": "Zincir", "form": "Beste", "nota": "", "icrâ": "", "güfte": ""},
+            { "id": 1, "eser": "Ağlar inler payine yüzler sürer gönlüm gözüm", "makam": "Eviç bûselik", "güftekâr": "Danişli Ali Dede (İlmî)", "bestekâr": "Hamâmîzâde İsmâil Dede Efendi", "usûl": "Çenber (ağır)", "form": "Beste", "nota": "", "icrâ": "", "güfte": ""},
+            { "id": 1, "eser": "Ağlatırlar güldürürler çeşmim yaşın sildirirler ", "makam": "Uşşak", "güftekâr": "Hamâmîzâde İsmâil Dede Efendi", "bestekâr": "Hamâmîzâde İsmâil Dede Efendi", "usûl": "Aksak", "form": "Şarkı", "nota": "", "icrâ": "", "güfte": ""},
+            { "id": 1, "eser": "Ağlatma beni incitme aman", "makam": "Beyati", "güftekâr": "Mechul", "bestekâr": "Hamâmîzâde İsmâil Dede Efendi", "usûl": "Aksak Semai", "form": "Şarkı", "nota": "", "icrâ": "", "güfte": ""},
+            { "id": 1, "eser": "Ah eylediğim sevr-i hıramanın içindir", "makam": "Isfahan", "güftekâr": "Fuzûlî", "bestekâr": "Hamâmîzâde İsmâil Dede Efendi", "usûl": "Yürük Semai", "form": "Şarkı", "nota": "", "icrâ": "", "güfte": ""},
+            { "id": 1, "eser": "Aklın alır aşıkların deleyler", "makam": "Beyati Araban", "güftekâr": "Mechul", "bestekâr": "Hamâmîzâde İsmâil Dede Efendi", "usûl": "Aksak", "form": "Şarkı", "nota": "", "icrâ": "", "güfte": ""},
+            { "id": 1, "eser": "Aman felek ömrüm felek", "makam": "Uşşak", "güftekâr": "Mechul", "bestekâr": "Hamâmîzâde İsmâil Dede Efendi", "usûl": "Aksak", "form": "Şarkı", "nota": "", "icrâ": "", "güfte": ""},
+            { "id": 1, "eser": "Arz-ı halim benim lutf-i dilbere kalsın", "makam": "Beyati Araban", "güftekâr": "Mechul", "bestekâr": "Hamâmîzâde İsmâil Dede Efendi", "usûl": "Ağır Düyek", "form": "Şarkı", "nota": "", "icrâ": "", "güfte": ""},
+        ]
